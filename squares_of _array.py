f9f71b2f207c04bf457004d171fdf594349e9841
EastSahara <69398632+EastSahara@users.noreply.github.com>
2022-05-31 00:24:43 -0400

    Add files via upload

---
+++ squares_of _array.py
@@ -0,0 +1,14 @@
+
+#leetcode 977
+numbers = [-4,-1,0, 3, 10]
+
+prime_numbers = [11, 3, 7, 5, 2]
+
+
+def sorted_array(numbers):
+    num = [x ** 2 for x in numbers ]
+    return list(num)
+
+
+
+#print(sorted_array(numbers))
